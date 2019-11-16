88d51db45997060872a181a29bfc2aca11aba8bb
Ilona <ilona.j5@wp.pl>
2019-11-16 23:35:29 +0100

    [add] - show customized Toast

--- app/src/main/java/com/example/lab7/MainActivity.java
+++ app/src/main/java/com/example/lab7/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.lab7;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    public void showToastOnClick(View view) {
+        Toast toast = new Toast(getApplicationContext());
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.AXIS_PULL_BEFORE,10,100);
+
+        View toastView = getLayoutInflater().inflate(R.layout.toast_view, null);
+        TextView text = toastView.findViewById(R.id.toastTextView);
+        ImageView imageView = toastView.findViewById(R.id.imageView);
+        text.setText("wow much customization!");
+        imageView.setImageResource(R.drawable.toast);
+        toast.setView(toastView);
+        toast.show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
